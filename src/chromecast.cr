5dc6dd0ac056426c8bbfd45eb67333c0d31ecf97
unknown <john@johnmurphy.me>
2022-01-11 18:00:45 +1100

    cleanup dead code

--- src/chromecast.cr
+++ src/chromecast.cr
@@ -64,7 +64,6 @@ class Castblock::Chromecast
 
     if !response.status.success?
       Log.debug &.emit("Error with skipad", status_code: response.status_code, error: response.body)
-      # raise CommandError.new 
       # We do not want to raise an error based off the response from /skipad as the ad may already be skipped.
       # The /skipad logic is run 30 times over 60 seconds until the ad is skipped, so subsequent requests as the watcher loops will fail
     end
